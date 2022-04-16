3a21d476bed82113285227ca0c213001d00510e7
daniel_marques <94077581+danielarques@users.noreply.github.com>
2022-04-16 12:32:02 -0300

    Add files via upload

---
+++ projeto 43/js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+    fill("white")
+    textSize(15)
+    text("a", 700,150)
+    fill("lightblue")
+    text("Dica: Performa uma tarefa em particular!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("a", 100,250)
+    fill("lightblue")
+    text("Dica: Armazena todas as informações!", 100,270)
+}
